f1fcc99461dc57f5978f38654e503b8c6846a89a
TharinduMadhusanka <108868341+TharinduMadhusanka@users.noreply.github.com>
2023-08-22 10:18:10 +0530

    change config data

--- client/src/googlesignin/config.js
+++ client/src/googlesignin/config.js
@@ -7,7 +7,17 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyBHtgDg35s3egKqDBnz-QiXIAnpWL8ubl8",
+  authDomain: "learning-3419a.firebaseapp.com",
+  databaseURL:
+    "https://learning-3419a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "learning-3419a",
+  storageBucket: "learning-3419a.appspot.com",
+  messagingSenderId: "820732401908",
+  appId: "1:820732401908:web:b61096c29435eeb49ca6a1",
+  measurementId: "G-MDZ1DK8YLG",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
